e2441fe8a6353e332740f0b6820db53cca018bcc
carterdy <selkie_link@hotmail.com>
2016-02-05 23:36:16 -0500

    Simple movement works. Players can jump and toggle the active player. Ice and fire objects can be jumped on depending on the active player

---
+++ Tandem/Assets/Scripts/ArcherBottomController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArcherBottomController : MonoBehaviour {
+
+    public float speed = 4f;
+    public float jumpSpeed = 5f;
+
+    private Rigidbody rb;
+    private Vector3 movement;
+    private float rayLength = 100f;
+    private int jumpable;
+
+    // Use this for initialization
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        jumpable = LayerMask.GetMask("Jumpable");
+    }
+
+    void FixedUpdate()
+    {
+        //Need to get movement axis values and hand them off to a movement function
+        float horizontal = Input.GetAxisRaw("Horizontal2");
+        float vertical = Input.GetAxisRaw("Vertical2");
+        if (Input.GetButton("Jump2"))
+        {
+            AttemptJump();
+        }
+
+        Move(horizontal, vertical);
+    }
+
+    /* Attempts to make the character jump.  Successful if the character is standing on a jumpable object */
+    void AttemptJump()
+    {
+        //Going to cast a ray down from the character, and we know they're standing on something jumpable if the
+        //difference between the ray hit and origin is 0
+
+        RaycastHit hit;
+        if (Physics.Raycast(gameObject.transform.position, -Vector3.up, out hit, rayLength, jumpable))
+        {
+            Debug.Log("Raycast hit");
+            Debug.Log(hit.distance);
+            if (hit.distance - gameObject.transform.localScale.y <= 0.0001)
+            {
+                Debug.Log(hit.distance);
+                rb.velocity = new Vector3(0f, jumpSpeed, 0f);
+            }
+        }
+    }
+
+    /* Move the player based off the given horizontal and vertical inputs */
+    void Move(float horizontal, float vertical)
+    {
+        movement.Set(horizontal, 0f, vertical);
+        movement = movement.normalized * speed * Time.deltaTime;
+        rb.MovePosition(transform.position + movement);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Tandem/Assets/Scripts/CentralPlayerController.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CentralPlayerController : MonoBehaviour {
+
+    public GameObject warrior;
+    public GameObject archer;
+    public static CentralPlayerController instance = null;
+
+    //The boolean determining who is on bottom.  If it's not the warrior, it's the archer.
+    private bool warriorBottom = true;
+
+
+	// Use this for initialization
+	void Awake () {
+        //Borrowing simple singleton code from Unity's Roguelike tutorial
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    /*Flip the players by disabling movement for the top player and changing the interactable objects */
+    void FlipPlayers ()
+    {
+        warriorBottom = !warriorBottom;
+        GameObject[] iceObstacles = GameObject.FindGameObjectsWithTag("IceObstacle");
+        GameObject[] fireObstacles = GameObject.FindGameObjectsWithTag("FireObstacle");
+        //Set the movement controller scripts
+        if (warriorBottom)
+        {
+            warrior.GetComponent<WarriorBottomController>().enabled = true;
+            archer.GetComponent<ArcherBottomController>().enabled = false;
+        } else
+        {
+            archer.GetComponent<ArcherBottomController>().enabled = true;
+            warrior.GetComponent<WarriorBottomController>().enabled = false;
+        }
+        //Enable/disable the colliders on the elemental objects
+        foreach (GameObject obstacle in iceObstacles)
+        {
+            if (warriorBottom)
+            {
+                obstacle.GetComponents<BoxCollider>()[1].enabled = false;
+            } else
+            {
+                obstacle.GetComponents<BoxCollider>()[1].enabled = true;
+            }
+        }
+        foreach (GameObject obstacle in fireObstacles)
+        {
+            if (warriorBottom)
+            {
+                obstacle.GetComponents<BoxCollider>()[1].enabled = true;
+            } else
+            {
+                obstacle.GetComponents<BoxCollider>()[1].enabled = false;
+            }
+        }
+    }
+
+    void FixedUpdate ()
+    {
+        if (Input.GetButtonUp("Fire1"))
+        {
+            FlipPlayers();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
