80c12b53e9c9d7279aaa45bf1c76831be41a04bd
carterdy <selkie_link@hotmail.com>
2016-03-01 14:27:16 -0500

    Blocking with moving shield partially works. Shield is limited in movement but always around the player on a global scale

--- Tandem/Assets/Scripts/PlayerScripts/WarriorTopController.cs
+++ Tandem/Assets/Scripts/PlayerScripts/WarriorTopController.cs
@@ -6,12 +6,15 @@ public class WarriorTopController : PlayerTopScript {
     public float shieldRotateSpeed = 5f;
 
     private GameObject activeShield;
+    //Min and max degrees the shield is allowed to rotate around the player. In Euler angles so it's weird
+    private float shieldRotateMin = 90f;
+    private float shieldRotateMax = 270f;
 
     void FixedUpdate()
     {
         float turn = Input.GetAxisRaw("Horizontal");
         Turn(turn);
-        if (Input.GetButton("Block"))
+        if (Input.GetAxis("Block") > 0)
         {
             Block();
         } else if (!Input.GetButton("Block")) {
@@ -28,13 +31,17 @@ public class WarriorTopController : PlayerTopScript {
             activeShield = Instantiate(shield);
             activeShield.transform.SetParent(gameObject.transform);
             activeShield.transform.localPosition = new Vector3(0f, 0.5f, 0.5f);
+            activeShield.transform.rotation = gameObject.transform.rotation;
         }
-        double vertical = Input.GetAxis("ShieldVertical");
-        double horizontal = Input.GetAxis("ShieldHorizontal");
-        double shieldAngle = (Math.Sin(horizontal)) * (180.0 / Math.PI);
-        activeShield.transform.Rotate(Vector3.up * shieldRotateSpeed * (float)horizontal);
-        //activeShield.transform.RotateAround(gameObject.transform.position, Vector3.up, shieldRotateSpeed * (float)horizontal);
-        Debug.Log(shieldAngle);
+        float horizontal = Input.GetAxis("ShieldHorizontal");
+        float activeShieldRotation = activeShield.transform.rotation.eulerAngles.y;
+        float playerRotation = gameObject.transform.rotation.eulerAngles.y;
+        if ((activeShieldRotation < shieldRotateMin || (horizontal > 0 && (activeShieldRotation - 180 > 0))) || (activeShieldRotation > shieldRotateMax || (horizontal < 0 && (activeShieldRotation - 180 < 0))))
+        {
+            activeShield.transform.RotateAround(gameObject.transform.position, Vector3.up, shieldRotateSpeed * horizontal);
+            Debug.Log(horizontal);
+            Debug.Log(activeShield.transform.rotation.eulerAngles.y);
+        }
     }
     
     /* Cease blocking by destroying the shield */
