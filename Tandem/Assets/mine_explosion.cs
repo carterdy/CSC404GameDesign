7ec019cb126fba90d7172f23f8ece2ba9d2b0c2d
maggg <maggie_shang25@hotmail.com>
2016-03-01 02:28:39 -0500

    Simple mine prefab
    cause explosion when been stepped on (constant scaling) then the
    explosion (object) is destroyed after 3 sec

---
+++ Tandem/Assets/mine_explosion.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mine_explosion : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+
+    void OnCollisionEnter(Collision col)
+    {
+        transform.localScale += new Vector3(3, 3, 3);
+        StartCoroutine(Example());
+        
+    }
+
+    IEnumerator Example()
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(this.gameObject);
+    }
+
+}
