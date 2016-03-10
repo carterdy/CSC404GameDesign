b1a8505916d00e96728404eed6aad9ff66c892bd
Dylan Carter <d.carter@mail.utoronto.ca>
2016-03-10 13:23:49 -0500

    Players can no longer move wile jumping

--- Tandem/Assets/Scripts/PlayerScripts/ArcherBottomController.cs
+++ Tandem/Assets/Scripts/PlayerScripts/ArcherBottomController.cs
@@ -12,7 +12,10 @@ public class ArcherBottomController : PlayerBottomScript {
     void FixedUpdate()
     {
         //Need to get movement axis values and hand them off to a movement function
-        float vertical = Input.GetAxisRaw("Vertical2");
+        if (isGrounded())
+        {
+            vertical = Input.GetAxisRaw("Vertical2");
+        }
         if (Input.GetButton("Jump2"))
         {
             AttemptJump();
