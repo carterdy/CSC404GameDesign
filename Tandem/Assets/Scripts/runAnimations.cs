9787c3ff734057cb50f18a2e3b893e912c2df949
g2huda <huda.fouda@mail.utoronto.ca>
2016-03-13 05:59:45 -0400

    added running animations

---
+++ Tandem/Assets/Scripts/runAnimations.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class runAnimations : MonoBehaviour {
+
+    private Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+
+    void FixedUpdate()
+    {
+        bool warriorActive = gameObject.GetComponent<WarriorBottomController>().isActiveAndEnabled;
+        //bool warriorJumping = Input.GetButton("Jump");
+        bool warriorGrounded = gameObject.GetComponent<WarriorBottomController>().isGrounded();
+        float warriorMove = Mathf.Abs(Input.GetAxisRaw("Vertical"));
+
+        bool archerActive = gameObject.GetComponent<ArcherBottomController>().isActiveAndEnabled;
+        //bool archerJumping = Input.GetButton("Jump2");
+        bool archerGrounded = gameObject.GetComponent<ArcherBottomController>().isGrounded();
+        float archerMove = Mathf.Abs(Input.GetAxisRaw("Vertical2"));
+
+        Debug.Log(archerMove);
+        string debug = "Active:" + archerActive + "\n" + "Grounded:" + archerGrounded + "\n" + "Moving:" + archerMove + "\n";
+        Debug.Log(debug);
+
+        if (warriorActive && warriorGrounded && warriorMove > 0.0f)
+        {
+            anim.SetInteger("run", 1);
+        }
+        else if (archerActive && archerGrounded && archerMove > 0.0f)
+        {
+            anim.SetInteger("run", 1);
+        }
+        else
+        {
+            anim.SetInteger("run", 0);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+}
