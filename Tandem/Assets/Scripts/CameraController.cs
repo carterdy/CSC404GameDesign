c34932607cb6468947973af387675c63607708ad
Dylan Carter <d.carter@mail.utoronto.ca>
2016-03-12 22:08:46 -0500

    Added partiallu working camera rotation

--- Tandem/Assets/Scripts/CameraController.cs
+++ Tandem/Assets/Scripts/CameraController.cs
@@ -5,7 +5,8 @@ public class CameraController : MonoBehaviour {
 
     /* Borrowing the method for camera control from Unity's Training Day tutorial */
     public Transform player;
-    public float smoothing = 5f;
+    public float positionSmoothing = 5f;
+    public float rotationSmoothing = 5f;
 
     public Vector3 offset = new Vector3(0f, 7f, -7f);
 
@@ -16,6 +17,8 @@ public class CameraController : MonoBehaviour {
 	
 	void FixedUpdate () {
         Vector3 targetCameraPos = player.position + offset;
-        transform.position = Vector3.Lerp(transform.position, targetCameraPos, smoothing * Time.deltaTime);
+        Quaternion targetCamRot = Quaternion.Euler(transform.rotation.x, player.rotation.y, transform.rotation.z);
+        transform.position = Vector3.Lerp(transform.position, targetCameraPos, positionSmoothing * Time.deltaTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetCamRot, rotationSmoothing * Time.deltaTime);
 	}
 }
