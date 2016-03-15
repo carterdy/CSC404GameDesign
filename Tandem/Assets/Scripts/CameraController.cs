8c2c51c32ed36028fa7c4471783d9ccb7d226391
carterdy <selkie_link@hotmail.com>
2016-03-15 16:40:32 -0400

    Camera now follows behind the player

--- Tandem/Assets/Scripts/CameraController.cs
+++ Tandem/Assets/Scripts/CameraController.cs
@@ -6,8 +6,6 @@ public class CameraController : MonoBehaviour {
     /* Borrowing the method for camera control from Unity's Training Day tutorial */
     public Transform player;
     public float positionSmoothing = 5f;
-    public float rotationSmoothing = 5f;
-
     public Vector3 offset = new Vector3(0f, 7f, -7f);
 
     // Use this for initialization
@@ -16,10 +14,10 @@ public class CameraController : MonoBehaviour {
 	}
 	
 	void FixedUpdate () {
-        Vector3 targetCameraPos = player.position + player.transform.InverseTransformVector(offset);
-        Quaternion targetCamRot = Quaternion.Euler(transform.rotation.x, player.rotation.y, transform.rotation.z);
+        float playerAngle = player.transform.eulerAngles.y;
+        Quaternion targetCameraRot = Quaternion.Euler(0, playerAngle, 0);
+        Vector3 targetCameraPos = player.position + (targetCameraRot * offset);
         transform.position = Vector3.Lerp(transform.position, targetCameraPos, positionSmoothing * Time.deltaTime);
-        //transform.rotation = Quaternion.Lerp(transform.rotation, targetCamRot, rotationSmoothing * Time.deltaTime);
         transform.LookAt(player.transform);
 	}
 }
