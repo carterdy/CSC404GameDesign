0fe1db5dc77c8f5f6db85208888426402b6f7f8b
carterdy <selkie_link@hotmail.com>
2016-03-16 20:17:16 -0400

    Added script to deactivate objects shot by arrows

---
+++ Tandem/Assets/Scripts/ObstacleScripts/ShootToDeactivate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* The purpose of this script is to attach it to some object that is shot by the players (like a switch).
+    When the object is shot, the paired object objectToDeactivate will be deactivated.  */
+
+public class ShootToDeactivate : MonoBehaviour {
+
+    public GameObject objectToDeactivate;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnCollisionEnter (Collision other)
+    {
+        if (other.gameObject.tag == "arrow")
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
