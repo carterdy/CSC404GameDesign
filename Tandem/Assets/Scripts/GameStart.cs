fe6ddebbbe8ff1cfe9caced5ac478b4b1b85e152
StevenDao <nataku235@gmail.com>
2016-03-22 01:10:53 -0400

    Added Level Transitions

--- Tandem/Assets/Scripts/GameStart.cs
+++ Tandem/Assets/Scripts/GameStart.cs
@@ -1,19 +1,37 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class GameStart : MonoBehaviour {
 
+	static int currentLevel = 1;
+
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	    if (Input.GetButton("Start"))
         {
-            SceneManager.LoadScene("AlphaLevel1");
+			incrementLevel ();
+			// Minus 3 for the non level scene files
+			// Title, Victory and GameOver
+			if (currentLevel <= Application.levelCount - 3) {
+				SceneManager.LoadScene ("Level" + currentLevel);
+			} 
+			
+			// Restart Title Screen
+			else {
+				SceneManager.LoadScene ("TitleScreen");
+			}
         }
+	}
+
+	// Safe level increment
+	void incrementLevel() {
+		if (Application.loadedLevelName == "Victory") {
+			currentLevel++;
+		}
 	}
 }
