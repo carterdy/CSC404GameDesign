5314eb069e23b5368f1418a27fb8996f056ddcde
g2huda <huda.fouda@mail.utoronto.ca>
2016-03-30 05:52:57 -0400

    adjusted the cross hair for better shooting

--- Tandem/Assets/Scripts/rayCastShooting.cs
+++ Tandem/Assets/Scripts/rayCastShooting.cs
@@ -1,43 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class rayCastShooting : MonoBehaviour {
+public class rayCastShooting : MonoBehaviour
+{
 
     public float distance = 40.0f;
-    public float cursorDistance = 9.0f;
+    public float cursorDistance = 5.0f;
     public bool debug = false;
 
-    private GameObject aim;
     private GameObject cursor;
     private SpriteRenderer cursorColor;
     private Vector3 startPos;
 
-	// Use this for initialization
-	void Start () {
-        aim = GameObject.Find("rayAim");
+    // Use this for initialization
+    void Start()
+    {
         cursor = GameObject.Find("Cursor");
         cursorColor = cursor.GetComponent<SpriteRenderer>();
-        startPos = new Vector3(cursorDistance/10, 0, 0);
+        startPos = new Vector3(cursorDistance / 10, 0, 0);
         cursor.transform.localPosition = startPos;
+    }
 
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Vector3 forward = aim.transform.TransformDirection(Vector3.down);
-        if(debug) Debug.DrawRay(transform.position, forward * distance, Color.red);
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.right);
+        forward.y = 0.0f;
+
+        Vector3 cursorForward = cursor.transform.TransformDirection(Vector3.forward);
+        cursorForward.y = 0;
+        Vector3 dir = cursor.transform.position - transform.position;
+        dir.y = 0;
+        cursor.transform.rotation = Quaternion.LookRotation(dir, Vector3.forward);
+
+        if (debug) Debug.DrawRay(transform.position, forward * distance, Color.red);
         RaycastHit enemy = new RaycastHit();
         bool hit = Physics.Raycast(transform.position, forward, out enemy, distance);
 
         if (hit)
         {
             if (debug) Debug.Log(enemy.transform.gameObject.name);
-            if (enemy.transform.gameObject.layer != 10) cursor.transform.localPosition = new Vector3((enemy.distance/10)-0.01f, 0, 0);
-            if(enemy.transform.gameObject.tag == "Target")
+            if (enemy.transform.gameObject.layer != 10) cursor.transform.localPosition = new Vector3((enemy.distance / 10) - 0.01f, 0, 0);
+            if (enemy.transform.gameObject.tag == "Target")
             {
                 cursorColor.color = Color.red;
-                
+
             }
             else
             {
@@ -48,6 +55,7 @@ public class rayCastShooting : MonoBehaviour {
         {
             cursor.transform.localPosition = startPos;
             cursorColor.color = Color.white;
-        }	
-	}
+        }
+        cursor.transform.position = new Vector3(cursor.transform.position.x, transform.position.y, cursor.transform.position.z);
+    }
 }
