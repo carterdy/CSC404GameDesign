c66899bf38e305014b4b994f098c82971493526b
StevenDao <nataku235@gmail.com>
2016-03-30 21:47:54 -0400

    Added button shoot, still a little buggy
    Add the button for shooting in the input manage and name it 'FireArrow'

--- Tandem/Assets/Scripts/PlayerScripts/ArcherTopController.cs
+++ Tandem/Assets/Scripts/PlayerScripts/ArcherTopController.cs
@@ -9,6 +9,7 @@ public class ArcherTopController : PlayerTopScript {
 	GameObject arrowPrefab;
 	GameObject player;
 	GameObject arrow;
+	GameObject shotArrow;
 
 	private float stickDir;
 	private Vector2 stickInput;
@@ -18,35 +19,30 @@ public class ArcherTopController : PlayerTopScript {
 	private float leftRange;
 	private float rightRange;
 
+	float cooldown = 1f;		
+	float shotTime;
+
     public AudioClip shootSound;
     private AudioSource source;
 
 
 	void Start() {
+		shotTime = 0;
 		arrowPrefab = Resources.Load ("Arrow") as GameObject;
 		player = GameObject.Find("CompletePlayer");
 		arrow = null;
+		shotArrow = null;
+
         //audio setup
         source = GetComponent<AudioSource>();
 	}
 
 	void FixedUpdate()
 	{
-		/* Rotate the players based off the given input */
-		float turn = Input.GetAxisRaw("Horizontal2");
-        if (turn != 0)
-        {
-            ArcherTurningActive.SetActive(true);
-            Turn(turn);
-        }
-        else
-        {
-            ArcherTurningActive.SetActive(false);
-        }
-
         // Aim Axis
         float aimX = Input.GetAxis("AimX");
 		float aimY = Input.GetAxis ("AimY");
+		float fire = Input.GetAxis ("FireArrow");
 
 		// Math to convert Axis to Angle Direction
 		stickDir = Mathf.Atan2 (aimX, aimY) * Mathf.Rad2Deg;
@@ -55,18 +51,17 @@ public class ArcherTopController : PlayerTopScript {
 		stickInput = new Vector2(aimX, aimY);
 
 		// If joystick is active
-		if (stickInput.magnitude > deadzone) {
+		if (stickInput.magnitude > deadzone && canShoot()) {
 			Aim (aimX, aimY);
-		} 
-		// Fire on X-Axis Release
-		else {
-			// Store the arrow in the arrow list
-			if (arrow != null) {
-				// Calls Destroy on the arrow
+			if (fire == 1) {
 				Shoot (arrow);
-				// Reset arrow, for next arrow to be shot
-				arrow = null;
+				shotTime = Time.time + cooldown;
 			}
+		} 
+		// Destroy arrow when on X-Axis Release
+		else if (arrow) {
+			Destroy (arrow.gameObject);
+			arrow = null;
 		}
 	}
 
@@ -90,10 +85,12 @@ public class ArcherTopController : PlayerTopScript {
 		// Make new vector for the arrow position
 		Vector3 newPos = new Vector3 (newX, 0, newZ);
 
-        Physics.IgnoreLayerCollision(9, 10, true);
-       // Physics.Ignore;
-        // Move the arrow into place
+		// Ignores layers passed as ints
+		// Player = 9
+		// Weapon = 10
+		Physics.IgnoreLayerCollision(9, 10, true);
 
+        // Move the arrow into place
         arrow.transform.position = hand.position - newPos;
     }
 
@@ -106,12 +103,17 @@ public class ArcherTopController : PlayerTopScript {
         //shoot sound
         source.PlayOneShot(shootSound, 1F);
 
+		// Save Arrow as Temp		
+		shotArrow = Instantiate(arrow);		
+		// Reset NULL arrow		
+		arrow = null;		
 		// Destroy the Arrow After 3 Seconds
-		Destroy(arrow.gameObject, 3);
+		Destroy(shotArrow.gameObject, 3);
 	}
 
 	void Aim (float aimX, float aimY) {
 		// if arrow does not exist
+		// And cooldown is passed				if (arrow == null) {
 		if (arrow == null) {
 			// Init Arrow
 			CreateArrow ();
@@ -192,6 +194,10 @@ public class ArcherTopController : PlayerTopScript {
 				UpdatePosition (arrowRot);
 			}
 		}
+	}
+
+	bool canShoot() {		
+		return shotTime <= Time.time;		
 	}
 	/*
 	 * Arrow Related Code End
