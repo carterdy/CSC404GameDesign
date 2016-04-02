cac15bf05771f96618d01e532b5db45750a638ac
StevenDao <nataku235@gmail.com>
2016-04-01 21:48:37 -0400

    Implemented Shoot Button! Refactored Code...

--- Tandem/Assets/Scripts/PlayerScripts/ArcherTopController.cs
+++ Tandem/Assets/Scripts/PlayerScripts/ArcherTopController.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class ArcherTopController : PlayerTopScript {
+public class ArcherTopController : PlayerTopScript
+{
 
-    public GameObject ArcherTurningActive;
-    public Transform hand;
+	public GameObject ArcherTurningActive;
+	public Transform hand;
+
+	private GameObject dummy;
 	GameObject arrowPrefab;
 	GameObject player;
 	GameObject arrow;
@@ -15,32 +18,44 @@ public class ArcherTopController : PlayerTopScript {
 	private Vector2 stickInput;
 	private float deadzone = 0.25f;
 
+	// Boolean to see if Fire is being 'held'
+	private bool firePressed;
+
 	private float aimRange = 90f;
 	private float leftRange;
 	private float rightRange;
 
-	float cooldown = 1f;		
-	float shotTime;
-
-    public AudioClip shootSound;
-    private AudioSource source;
+	public AudioClip shootSound;
+	private AudioSource source;
 
 
-	void Start() {
-		shotTime = 0;
+	void Start ()
+	{
 		arrowPrefab = Resources.Load ("Arrow") as GameObject;
-		player = GameObject.Find("CompletePlayer");
-		arrow = null;
-		shotArrow = null;
+		player = GameObject.Find ("CompletePlayer");
+		// null checker is broken
+		// dummy used instead of null checker
+		dummy = new GameObject ();
+		arrow = dummy;
+		firePressed = false;
 
-        //audio setup
-        source = GetComponent<AudioSource>();
+		//audio setup
+		source = GetComponent<AudioSource> ();
 	}
 
-	void FixedUpdate()
+	void FixedUpdate ()
 	{
-        // Aim Axis
-        float aimX = Input.GetAxis("AimX");
+		/* Rotate the players based off the given input */
+		float turn = Input.GetAxisRaw ("Horizontal2");
+		if (turn != 0) {
+			ArcherTurningActive.SetActive (true);
+			Turn (turn);
+		} else {
+			ArcherTurningActive.SetActive (false);
+		}
+
+		// Aim Axis
+		float aimX = Input.GetAxis ("AimX");
 		float aimY = Input.GetAxis ("AimY");
 		float fire = Input.GetAxis ("FireArrow");
 
@@ -48,20 +63,34 @@ public class ArcherTopController : PlayerTopScript {
 		stickDir = Mathf.Atan2 (aimX, aimY) * Mathf.Rad2Deg;
 
 		// Used for Deadzone Check
-		stickInput = new Vector2(aimX, aimY);
+		stickInput = new Vector2 (aimX, aimY);
 
 		// If joystick is active
-		if (stickInput.magnitude > deadzone && canShoot()) {
+		if (stickInput.magnitude > deadzone) {
 			Aim (aimX, aimY);
 			if (fire == 1) {
-				Shoot (arrow);
-				shotTime = Time.time + cooldown;
+				// Prevent arrows from firing like a machine gun
+				// Check if fire was pressed or held
+				if (!firePressed) {
+					firePressed = true;
+					Shoot (arrow);
+					arrow = dummy;
+				} 
+			}
+			// When player releases button (Not being Held)
+			else if (fire == 0 || fire == -1) {
+				// Reset firePressed Variable
+				firePressed = false;
 			}
 		} 
-		// Destroy arrow when on X-Axis Release
-		else if (arrow) {
-			Destroy (arrow.gameObject);
-			arrow = null;
+		// When Aim Axes are not active reset everything
+		else { 
+			if (arrow != dummy) {
+				Destroy (arrow.gameObject);
+				arrow = dummy;
+			}
+			// Reset firePressed Variable
+			firePressed = false;
 		}
 	}
 
@@ -69,72 +98,40 @@ public class ArcherTopController : PlayerTopScript {
 	 * Arrow Related Code Start
 	 */
 
-	void CreateArrow() {
+	void CreateArrow ()
+	{
 		arrow = Instantiate (arrowPrefab) as GameObject;
 	}
 
-	void UpdatePosition(float angle) {
-		// Arrow Should Rotate Around Player
-		Vector3 playerPos = player.transform.position;
-		float rads = angle * Mathf.Deg2Rad;
-
-		// Math here...
-		// Rotation around Origin with Vector [0, 0, 0.5]
-		float newX = Mathf.Sin(rads) / 2;
-		float newZ = Mathf.Cos(rads) / 2;
-		// Make new vector for the arrow position
-		Vector3 newPos = new Vector3 (newX, 0, newZ);
-
-		// Ignores layers passed as ints
-		// Player = 9
-		// Weapon = 10
-		Physics.IgnoreLayerCollision(9, 10, true);
-
-        // Move the arrow into place
-        arrow.transform.position = hand.position - newPos;
-    }
-
-	void Shoot(GameObject arrow)
+	void Aim (float aimX, float aimY)
 	{
-		// Add Arrow Velocity, Driection of Arrow Head
-		Rigidbody rb = arrow.GetComponent<Rigidbody>();
-		rb.velocity = arrow.transform.up * 10;
-
-        //shoot sound
-        source.PlayOneShot(shootSound, 1F);
-
-		// Save Arrow as Temp		
-		shotArrow = Instantiate(arrow);		
-		// Reset NULL arrow		
-		arrow = null;		
-		// Destroy the Arrow After 3 Seconds
-		Destroy(shotArrow.gameObject, 3);
-	}
-
-	void Aim (float aimX, float aimY) {
 		// if arrow does not exist
-		// And cooldown is passed				if (arrow == null) {
-		if (arrow == null) {
+		// And fire is note held down
+		if (arrow == dummy && !firePressed) {
 			// Init Arrow
 			CreateArrow ();
 		}
 		// Arrow exist, now aim the arrow
 		else {
 			// Rotate Arrow in the direction of the Joystick
-			LimitAimCone();
+			LimitAimCone ();
 		}
 	}
 
-	void LimitStickDir(float angle) {
-		// Controller Direction angles are 180 to -180
-		// Convert rotation angle to controller Angle
-		if (angle > 180) {
-			angle = angle - 360;
-		}
-		arrow.transform.rotation = Quaternion.Euler (new Vector3 (90, angle, 0));
-	}
+	void Shoot (GameObject arrow)
+	{
+		// Add Arrow Velocity, Driection of Arrow Head
+		Rigidbody rb = arrow.GetComponent<Rigidbody> ();
+		rb.velocity = arrow.transform.up * 10;
 
-	void LimitAimCone() {
+		//shoot sound
+		source.PlayOneShot (shootSound, 1F);
+
+		Destroy (arrow.gameObject, 3);
+	}
+		
+	void LimitAimCone ()
+	{
 		float playerRotation = player.transform.eulerAngles.y;
 		float arrowRot = playerRotation + stickDir;
 
@@ -196,14 +193,43 @@ public class ArcherTopController : PlayerTopScript {
 		}
 	}
 
-	bool canShoot() {		
-		return shotTime <= Time.time;		
+	void LimitStickDir (float angle)
+	{
+		// Controller Direction angles are 180 to -180
+		// Convert rotation angle to controller Angle
+		if (angle > 180) {
+			angle = angle - 360;
+		}
+		arrow.transform.rotation = Quaternion.Euler (new Vector3 (90, angle, 0));
+	}
+
+	void UpdatePosition (float angle)
+	{
+		// Arrow Should Rotate Around Player
+		Vector3 playerPos = player.transform.position;
+		float rads = angle * Mathf.Deg2Rad;
+
+		// Math here...
+		// Rotation around Origin with Vector [0, 0, 0.5]
+		float newX = Mathf.Sin (rads) / 2;
+		float newZ = Mathf.Cos (rads) / 2;
+		// Make new vector for the arrow position
+		Vector3 newPos = new Vector3 (newX, 0, newZ);
+
+		// Ignores layers passed as ints
+		// Player = 9
+		// Weapon = 10
+		Physics.IgnoreLayerCollision (9, 10, true);
+
+		// Move the arrow into place
+		arrow.transform.position = hand.position - newPos;
 	}
 	/*
 	 * Arrow Related Code End
 	 */
-     void OnDisable()
-    {
-        if (arrow) Destroy(arrow);
-    }
+	void OnDisable ()
+	{
+		if (arrow)
+			Destroy (arrow);
+	}
 }
